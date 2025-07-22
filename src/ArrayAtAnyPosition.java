f302fe99949ed9f109a78324081565358de550a7
Abinash Mohapatra <mabinash016@gmail.com>
2025-07-22 11:54:12 +0530

    Insert at any positon

---
+++ src/ArrayAtAnyPosition.java
@@ -0,0 +1,10 @@
+public class ArrayAtAnyPosition {
+    public static void main(String[] args) {
+        int [] arr={14,15,18,56};
+        int newEle=34;
+
+        int [] updted=new int[arr.length+1];
+
+        
+    }
+}
